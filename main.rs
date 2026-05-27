a6127e4868d2b642a6676326e766f80ac55b90f1
Ludovic MARIE <91457982+LudovicMARIE@users.noreply.github.com>
2026-05-27 09:29:30 +0200

    feat: hello world + gitignore

---
+++ main.rs
@@ -0,0 +1,12 @@
+// This is a comment, and is ignored by the compiler.
+
+// This code is editable, feel free to hack it!
+// You can always return to the original code by clicking the "Reset" button ->
+
+// This is the main function.
+fn main() {
+    // Statements here are executed when the compiled binary is called.
+
+    // Print text to the console.
+    println!("Hello World!");
+}
